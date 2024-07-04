9b061c830becf445bde8b90e14458a138605d74b
PrabakaranN <140478966+Prabakarann12@users.noreply.github.com>
2024-07-04 18:36:35 +0530

    Update DocumentPage.dart

--- Flutter_Uniapp/DocumentPage.dart
+++ Flutter_Uniapp/DocumentPage.dart
@@ -12,56 +12,22 @@ class DocumentPage extends StatefulWidget {
 }
 
 class _DocumentPageState extends State<DocumentPage> {
-  List<double> _uploadProgress = List.filled(7, 0.0); // List to store upload progress for 7 sections
-  List<String?> _selectedFiles = List.filled(7, null); // List to store file names for 7 sections
+  double _uploadProgress = 0.0;
+  List<String?> _selectedFiles = List.filled(4, null); // List to store file names for 3 sections
   bool _fileUploaded = false;
   final TextEditingController _aadhaarController = TextEditingController();
   final TextEditingController _passportController = TextEditingController();
   bool _aadhaarSubmitted = false;
   bool _passportSubmitted = false;
-  String? _selectedCategory; // Variable to track user selection (UG or PG)
 
   Future<void> _pickDocument(int index) async {
-    // Show documents based on the selected category (UG or PG)
-    switch (_selectedCategory) {
-      case 'UG':
-        _showUGDocuments(index);
-        break;
-      case 'PG':
-        _showPGDocuments(index);
-        break;
-      default:
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Please select UG or PG first')),
-        );
-    }
-  }
-
-  // Show UG documents
-  void _showUGDocuments(int index) {
-    setState(() {
-      _selectedFiles = List.filled(2, null); // Only 10th and 12th marksheet for UG
-    });
-    _pickFile(index);
-  }
-
-  // Show PG documents
-  void _showPGDocuments(int index) {
-    setState(() {
-      _selectedFiles = List.filled(3, null); // Include UG + PG documents
-    });
-    _pickFile(index);
-  }
-
-  // Method to actually pick the file
-  Future<void> _pickFile(int index) async {
     try {
       final result = await FilePicker.platform.pickFiles();
       if (result != null) {
         final platformFile = result.files.first;
         final file = File(platformFile.path!);
         setState(() {
-          _selectedFiles[index] = platformFile.path;
+          _selectedFiles[index] = platformFile.name;
           _fileUploaded = false;
         });
         await _uploadFile(file, index);
@@ -136,7 +102,7 @@ class _DocumentPageState extends State<DocumentPage> {
 
   Future<void> _uploadFile(File file, int index) async {
     try {
-      final uri = Uri.parse('');
+      final uri = Uri.parse('https://syfer001testing.000webhostapp.com/cloneapi/Uni_docinsert.php');
       final request = http.MultipartRequest('POST', uri)
         ..files.add(http.MultipartFile.fromBytes('file', await file.readAsBytes(), filename: file.path.split('/').last))
         ..fields['id'] = Uuid().v4()
@@ -146,41 +112,25 @@ class _DocumentPageState extends State<DocumentPage> {
       final streamedResponse = await request.send();
       final response = await http.Response.fromStream(streamedResponse);
 
-      streamedResponse.stream.listen(
-            (value) {
-          setState(() {
-            _uploadProgress[index] += value.length / streamedResponse.contentLength!;
-          });
-        },
-        onDone: () async {
-          if (response.statusCode == 200) {
-            final responseData = response.body;
-            print('File uploaded successfully: $responseData');
-            setState(() {
-              _uploadProgress[index] = 1.0;
-              _fileUploaded = true;
-            });
-          } else {
-            print('File upload failed with status: ${response.statusCode}');
-            print('Response body: ${response.body}');
-            setState(() {
-              _uploadProgress[index] = 0.0;
-              _fileUploaded = false;
-            });
-          }
-        },
-        onError: (e) {
-          print('Error uploading file: $e');
-          setState(() {
-            _uploadProgress[index] = 0.0;
-            _fileUploaded = false;
-          });
-        },
-      );
+      if (response.statusCode == 200) {
+        final responseData = response.body;
+        print('File uploaded successfully: $responseData');
+        setState(() {
+          _uploadProgress = 1.0;
+          _fileUploaded = true;
+        });
+      } else {
+        print('File upload failed with status: ${response.statusCode}');
+        print('Response body: ${response.body}');
+        setState(() {
+          _uploadProgress = 0.0;
+          _fileUploaded = false;
+        });
+      }
     } catch (e) {
       print('Error uploading file: $e');
       setState(() {
-        _uploadProgress[index] = 0.0;
+        _uploadProgress = 0.0;
         _fileUploaded = false;
       });
     }
@@ -191,21 +141,13 @@ class _DocumentPageState extends State<DocumentPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "Document Verification",
-          style: const TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(
-            Icons.arrow_back_ios,
-            size: 20.0,
-            color: Colors.white,
+          "Document Upload",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
           ),
         ),
+        backgroundColor: Colors.white,
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -213,117 +155,55 @@ class _DocumentPageState extends State<DocumentPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              DropdownButton<String>(
-                value: _selectedCategory,
-                hint: Text('Select Category'),
-                onChanged: (value) {
-                  setState(() {
-                    _selectedCategory = value;
-                    // Reset selected files when category changes
-                    _selectedFiles = List.filled(7, null);
-                  });
-                },
-                items: [
-                  DropdownMenuItem(child: Text('UG'), value: 'UG'),
-                  DropdownMenuItem(child: Text('PG'), value: 'PG'),
-                ],
+              DocumentUploadSection(
+                title: "10th Marksheet",
+                pickDocument: () => _pickDocument(0),
+                selectedFile: _selectedFiles[0],
               ),
-              SizedBox(height: 20),
-              if (_selectedCategory == 'UG' || _selectedCategory == 'PG')
-                DocumentUploadSection(
-                  title: "10th Marksheet",
-                  pickDocument: () => _pickDocument(0),
-                  selectedFile: _selectedFiles[0],
-                  progress: _uploadProgress[0],
-                ),
-              if (_selectedCategory == 'UG' || _selectedCategory == 'PG')
-                SizedBox(height: 10),
-              if (_selectedCategory == 'UG' || _selectedCategory == 'PG')
-                DocumentUploadSection(
-                  title: "12th Marksheet",
-                  pickDocument: () => _pickDocument(1),
-                  selectedFile: _selectedFiles[1],
-                  progress: _uploadProgress[1],
-                ),
-              if (_selectedCategory == 'PG')
-                SizedBox(height: 10),
-              if (_selectedCategory == 'PG')
-                DocumentUploadSection(
-                  title: "Any UG Certificate",
-                  pickDocument: () => _pickDocument(2),
-                  selectedFile: _selectedFiles[2],
-                  progress: _uploadProgress[2],
-                ),
+              SizedBox(height: 10),
+              DocumentUploadSection(
+                title: "12th Marksheet",
+                pickDocument: () => _pickDocument(1),
+                selectedFile: _selectedFiles[1],
+              ),
+              SizedBox(height: 10),
+              DocumentUploadSection(
+                title: "Any UG Certificate",
+                pickDocument: () => _pickDocument(2),
+                selectedFile: _selectedFiles[2],
+              ),
               SizedBox(height: 10),
               DocumentUploadSection(
                 title: "Medical Certificate",
                 pickDocument: () => _pickDocument(3),
                 selectedFile: _selectedFiles[3],
-                progress: _uploadProgress[3],
               ),
+
               SizedBox(height: 10),
-              DocumentUploadSection(
-                title: "Blood Certificate",
-                pickDocument: () => _pickDocument(4),
-                selectedFile: _selectedFiles[4],
-                progress: _uploadProgress[4],
-              ),
-              SizedBox(height: 10),
-              DocumentUploadSection(
-                title: "Extracurricular Activities Certificate",
-                pickDocument: () => _pickDocument(5),
-                selectedFile: _selectedFiles[5],
-                progress: _uploadProgress[5],
-              ),
-              SizedBox(height: 10),
-              DocumentUploadSection(
-                title: "Signature",
-                pickDocument: () => _pickDocument(6),
-                selectedFile: _selectedFiles[6],
-                progress: _uploadProgress[6],
-              ),
-              SizedBox(height: 20),
               AadhaarInputSection(
-                controller: _aadhaarController,
-                onChanged: () {
-                  setState(() {
-                    _aadhaarSubmitted = false;
-                  });
-                },
+                aadhaarController: _aadhaarController,
+                aadhaarSubmitted: _aadhaarSubmitted,
               ),
               SizedBox(height: 10),
               PassportInputSection(
-                controller: _passportController,
-                onChanged: () {
-                  setState(() {
-                    _passportSubmitted = false;
-                  });
-                },
+                passportController: _passportController,
+                passportSubmitted: _passportSubmitted,
               ),
               SizedBox(height: 20),
               ElevatedButton(
                 onPressed: _submit,
                 child: Text(
-                  'Upload',
+                  'Submit All',
                   style: TextStyle(color: Colors.white),
                 ),
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
-                ),
-              ),
-              SizedBox(height: 20),
-              if (_fileUploaded)
-                Center(
-                  child: Text(
-                    'Files and data uploaded successfully!',
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
+                  minimumSize: Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
                   ),
                 ),
-
+              ),
             ],
           ),
         ),
@@ -334,17 +214,14 @@ class _DocumentPageState extends State<DocumentPage> {
 
 class DocumentUploadSection extends StatelessWidget {
   final String title;
-  final VoidCallback pickDocument;
+  final Function() pickDocument;
   final String? selectedFile;
-  final double progress;
 
-  const DocumentUploadSection({
-    Key? key,
+  DocumentUploadSection({
     required this.title,
     required this.pickDocument,
     required this.selectedFile,
-    required this.progress,
-  }) : super(key: key);
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -353,38 +230,60 @@ class DocumentUploadSection extends StatelessWidget {
       children: [
         Text(
           title,
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
         ),
-        SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            ElevatedButton(
-              onPressed: pickDocument,
-              child: Text('Pick Document',style: TextStyle(color: Colors.white),),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-              ),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ElevatedButton(
+                  onPressed: pickDocument,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Pick Document',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      if (selectedFile != null)
+                        Padding(
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Icon(
+                            Icons.check_circle,
+                            color: Colors.green,
+                          ),
+                        ),
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                if (selectedFile != null)
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: Text(
+                      'Selected file: $selectedFile',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+              ],
             ),
-
-            if (selectedFile != null)
-              Container(
-                width: 150,
-                child: LinearProgressIndicator(
-                  value: progress,
-                  minHeight: 10,
-                ),
-              ),
-            if (selectedFile != null) SizedBox(width: 10),
-            if (selectedFile != null)
-              TextButton(
-                onPressed: () {},
-                child: Text(
-                  'Remove',
-                  style: TextStyle(color: Colors.red),
-                ),
-              ),
-          ],
+          ),
         ),
       ],
     );
@@ -392,14 +291,13 @@ class DocumentUploadSection extends StatelessWidget {
 }
 
 class AadhaarInputSection extends StatelessWidget {
-  final TextEditingController controller;
-  final VoidCallback onChanged;
+  final TextEditingController aadhaarController;
+  final bool aadhaarSubmitted;
 
-  const AadhaarInputSection({
-    Key? key,
-    required this.controller,
-    required this.onChanged,
-  }) : super(key: key);
+  AadhaarInputSection({
+    required this.aadhaarController,
+    required this.aadhaarSubmitted,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -407,16 +305,33 @@ class AadhaarInputSection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Aadhaar Number',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          "Aadhaar Input",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
         ),
-        SizedBox(height: 5),
-        TextField(
-          controller: controller,
-          onChanged: (_) => onChanged(),
-          decoration: InputDecoration(
-            hintText: 'Enter Aadhaar Number',
-            border: OutlineInputBorder(),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: aadhaarController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Aadhaar Number',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
@@ -425,14 +340,13 @@ class AadhaarInputSection extends StatelessWidget {
 }
 
 class PassportInputSection extends StatelessWidget {
-  final TextEditingController controller;
-  final VoidCallback onChanged;
+  final TextEditingController passportController;
+  final bool passportSubmitted;
 
-  const PassportInputSection({
-    Key? key,
-    required this.controller,
-    required this.onChanged,
-  }) : super(key: key);
+  PassportInputSection({
+    required this.passportController,
+    required this.passportSubmitted,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -440,16 +354,32 @@ class PassportInputSection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          'Passport Number',
-          style: TextStyle(fontWeight: FontWeight.bold),
+          "Passport Input",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 20.0,
+          ),
         ),
-        SizedBox(height: 5),
-        TextField(
-          controller: controller,
-          onChanged: (_) => onChanged(),
-          decoration: InputDecoration(
-            hintText: 'Enter Passport Number',
-            border: OutlineInputBorder(),
+        Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          elevation: 5,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: passportController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter Passport Number',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ],
