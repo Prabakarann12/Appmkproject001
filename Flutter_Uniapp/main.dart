816c047c51a7dbc83967a76c960726d53e1d3cd4
PrabakaranN <140478966+Prabakarann12@users.noreply.github.com>
2024-07-04 18:34:28 +0530

    Update main.dart

--- Flutter_Uniapp/main.dart
+++ Flutter_Uniapp/main.dart
@@ -1,18 +1,41 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'RegisterPage.dart';
+import 'package:local_auth/local_auth.dart';
 import 'SignInPage.dart';
-import 'SearchPage.dart';
-
+import 'RegisterPage.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: HomePage1(),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class HomePage1 extends StatefulWidget {
+  @override
+  _HomePage1State createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<HomePage1> {
+  final LocalAuthentication auth = LocalAuthentication();
+  bool _authenticated = false;
+
+  Future<void> _authenticate() async {
+    try {
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate to show login buttons',
+        options: AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+        ),
+      );
+      setState(() {
+        _authenticated = authenticated;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,13 +49,14 @@ class HomePage extends StatelessWidget {
             return Container(
               width: double.infinity,
               height: height,
-              padding: EdgeInsets.symmetric(horizontal: width * 0.05, vertical: height * 0.05),
+              padding: EdgeInsets.symmetric(horizontal: width * 0.05),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
                   Column(
                     children: <Widget>[
+                      SizedBox(height: height * 0.05),
                       Text(
                         "Welcome",
                         style: TextStyle(fontSize: width * 0.09, fontWeight: FontWeight.bold),
@@ -43,69 +67,89 @@ class HomePage extends StatelessWidget {
                         textAlign: TextAlign.center,
                         style: TextStyle(color: Colors.grey[700], fontSize: width * 0.04),
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(width * 0.05),
-                        child: Container(
-                          height: height * 0.4,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage('assets/WelcomeHomePG.png'),
-                              fit: BoxFit.cover,
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(width * 0.05),
+                    child: Container(
+                      height: height * 0.4,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/WelcomeHomePG.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    children: <Widget>[
+                      SizedBox(height: 25),
+                      Text(
+                        "Unistudy",
+                        style: TextStyle(fontSize: width * 0.09, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: height * 0.02),
+                      if (!_authenticated) ...[
+                        SizedBox(height: 10),
+                        Divider(color: Colors.black),
+                        SizedBox(height: 15),
+                        ElevatedButton.icon(
+                          onPressed: _authenticate,
+                          icon: Icon(Icons.fingerprint,color: Colors.black,),
+
+                          label: Text("Authenticate", style: TextStyle(color: Colors.black),),
+
+
+                        ),
+
+                        SizedBox(height: 20,)
+                      ],
+                      if (_authenticated) ...[
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: height * 0.02),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context)=> SignInPage())
+                              );
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.black), // background color
+                              foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // text color
+                              padding: MaterialStateProperty.all<EdgeInsets>(
+                                EdgeInsets.symmetric(horizontal: width * 0.30, vertical: height * 0.02), // padding
+                              ),
+                              textStyle: MaterialStateProperty.all<TextStyle>(
+                                TextStyle(fontSize: width * 0.04), // font size
+                              ),
                             ),
+                            child: Text('Login'),
                           ),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(bottom: height * 0.02),
-                        child: Text(
-                          "Unistudy",
-                          style: TextStyle(fontSize: width * 0.09, fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: height * 0.02,),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignInPage()),
-                            );
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.black), // background color
-                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // text color
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.symmetric(horizontal: width * 0.30, vertical: height * 0.02), // padding
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: height * 0.02),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context)=>RegisterPage())
+                              );
+                            },
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(Colors.black), // background color
+                              foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // text color
+                              padding: MaterialStateProperty.all<EdgeInsets>(
+                                EdgeInsets.symmetric(horizontal: width * 0.18, vertical: height * 0.02), // padding
+                              ),
+                              textStyle: MaterialStateProperty.all<TextStyle>(
+                                TextStyle(fontSize: width * 0.04), // font size
+                              ),
                             ),
-                            textStyle: MaterialStateProperty.all<TextStyle>(
-                              TextStyle(fontSize: width * 0.04), // font size
-                            ),
+                            child: Text("Create an account"),
                           ),
-                          child: Text('Login'),
                         ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: height * 0.02),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => RegisterPage()),
-                            );
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(Colors.black), // background color
-                            foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // text color
-                            padding: MaterialStateProperty.all<EdgeInsets>(
-                              EdgeInsets.symmetric(horizontal: width * 0.18, vertical: height * 0.02), // padding
-                            ),
-                            textStyle: MaterialStateProperty.all<TextStyle>(
-                              TextStyle(fontSize: width * 0.04), // font size
-                            ),
-                          ),
-                          child: Text("Create an account"),
-                        ),
-                      ),
+                      ],
                     ],
                   ),
                 ],
