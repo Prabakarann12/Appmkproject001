99a962f65374f0272b24bab8bcc231bfa7cd2bce
PrabakaranN <140478966+Prabakarann12@users.noreply.github.com>
2024-07-12 12:02:36 +0530

    Update NetworkPage.dart

--- Flutter_Uniapp/NetworkPage.dart
+++ Flutter_Uniapp/NetworkPage.dart
@@ -1,52 +1,81 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'Chatpage.dart';
 
 
 class NetworkPage extends StatelessWidget {
-  const NetworkPage({super.key});
+   NetworkPage({Key? key}) : super(key: key);
+   Future<List<dynamic>> fetchUniversities() async {
+     final response = await http.get(
+       Uri.parse('https://syfer001testing.000webhostapp.com/cloneapi/Universitylistdata.php'),
+     );
 
-  @override
-  Widget build(BuildContext context) {
-    List<String> imagePaths = [
-      'assets/a.png',
-      'assets/b.png',
-      'assets/c.png',
-      'assets/d.png',
-      'assets/e.png',
-      'assets/f.png',
-      'assets/g.png',
-      'assets/h.png',
-    ];
+     if (response.statusCode == 200) {
+       final jsonData = json.decode(response.body);
+       return jsonData['data'];
+     } else {
+       throw Exception('Failed to load data');
+     }
+   }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Network',style: TextStyle(color: Colors.white),),
-        backgroundColor: Colors.black87,
-      ),
-      body: ListView.builder(
-        itemCount: imagePaths.length,
-        itemBuilder: (context, index) {
-          return Card(
-            margin: const EdgeInsets.all(10),
-            child: ListTile(
-              contentPadding: const EdgeInsets.all(10),
-              leading: Image.asset(
-                imagePaths[index],
-                width: 50,
-                height: 50,
-                fit: BoxFit.cover,
-              ),
-              title: Text('Network Item $index'),
-              subtitle: Text('This is a description for item $index.'),
-              trailing: Icon(Icons.arrow_forward),
-              onTap: () {
-                // Handle card tap
-              },
-            ),
-          );
-        },
-      ),
-    );
-  }
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       appBar: AppBar(
+         title: const Text('College', style: TextStyle(color: Colors.white)),
+         backgroundColor: Colors.black87,
+         leading: IconButton(
+           onPressed: () {
+             Navigator.pop(context);
+           },
+           icon: Icon(
+             Icons.arrow_back_ios,
+             size: 20.0,
+             color: Colors.white,
+           ),
+         ),
+       ),
+
+       body: FutureBuilder<List<dynamic>>(
+         future: fetchUniversities(),
+         builder: (context, snapshot) {
+           if (snapshot.connectionState == ConnectionState.waiting) {
+             return const Center(child: CircularProgressIndicator(
+               valueColor: AlwaysStoppedAnimation<Color>(Colors.black),
+             ));
+           } else if (snapshot.hasError) {
+             return Center(child: Text('Error: ${snapshot.error}'));
+           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+             return const Center(child: Text('No data found'));
+           } else {
+             return ListView.builder(
+               itemCount: snapshot.data!.length,
+               itemBuilder: (context, index) {
+                 final university = snapshot.data![index];
+                 return Card(
+                   margin: const EdgeInsets.all(10),
+                   child: ListTile(
+                     contentPadding: const EdgeInsets.all(10),
+                     leading: Image.network(
+                       'https://syfer001testing.000webhostapp.com/cloneapi/${university['image_path']}',
+                       width: 50,
+                       height: 50,
+                       fit: BoxFit.cover,
+                     ),
+                     title: Text(university['universities_title']),
+                     subtitle: Text(university['universities_subtitle']),
+                     trailing: const Icon(Icons.arrow_forward),
+                     onTap: () {
+                       // Handle card tap
+                     },
+                   ),
+                 );
+               },
+             );
+           }
+         },
+       ),
+     );
+   }
 }
-
-
